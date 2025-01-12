0f2c3665710e5d5aed8b6b8425fc3c3d75e97ecf
Illia Raietskyi <rayetzkiillya@gmail.com>
2025-01-12 13:03:54 +0200

    Async start finish

--- async-start/stressor.js
+++ async-start/stressor.js
@@ -1,10 +1,8 @@
 'use strict'
 
 const fs = require('fs')
-const util = require('util')
-const stream = require('stream')
+const { pipeline, finished } = require('stream/promises')
 const path = require('path')
-const { once } = require('events')
 const saxophonist = require('saxophonist')
 const { createBrotliDecompress } = require('zlib')
 const pLimit = require('p-limit')
@@ -12,10 +10,6 @@ const pLimit = require('p-limit')
 const files = process.argv.splice(2)
 const pagesFilename = path.resolve(__dirname, 'pages.csv')
 
-const pipeline = util.promisify(stream.pipeline)
-
-// TODO
-// promisify stream.finished
 let total = 0
 
 const chunkToRow = (chunk) => {
@@ -26,13 +20,17 @@ const chunkToRow = (chunk) => {
 
 async function parseAsync(file, writable) {
   console.time(file)
-  const readable = fs.createReadStream(file, { encoding: 'utf8' })
+  const readable = fs.createReadStream(file)
   const brotli = createBrotliDecompress()
   const parseStream = saxophonist('page', {
     highWaterMark: 1024 * 1024 * 1024 * 32
   })
 
   async function * transform (stream) {
+    for await (const chunk of stream) {
+      total++
+      yield chunkToRow(chunk)
+    }
     // TODO
     // 1. iterate over each chunk
     // 2. increment the total
@@ -43,7 +41,14 @@ async function parseAsync(file, writable) {
   // TODO
   // 1. create a pipeline from readable -> brotli -> parseStream -> transform -> writable
   // 2. do not end the writable stream, so that it can be reused for the next parsed file
-  
+  await pipeline(
+    readable,
+    brotli,
+    parseStream,
+    transform,
+    writable,
+    { end: false }
+  )
   // TODO: Extra
   // 1. call `writable.write()` directly inside the transform function
   // 2. remove the yield and async generator function
@@ -62,6 +67,12 @@ async function start () {
   // 2. write the header to the file "id, title"
   // 3. Iterate over each file and call parseAsync in *series*
   // 3. End the writable stream and then wait on `finished(writeable)`
+  const pagesStream = fs.createWriteStream(pagesFilename)
+  pagesStream.write('id,title\n')
+  
+  for (const file of files) {
+    await parseAsync(file, pagesStream)
+  }
 
   // TODO: Extra - parallelise
   // 1. Map over each file and call parseAsync
@@ -70,6 +81,9 @@ async function start () {
 
   console.log('total: ', total)
   console.timeEnd('parsing time')
+
+  pagesStream.end()
+  await finished(pagesStream)
 }
 
 start()
