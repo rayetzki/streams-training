e94bd7e629188035b320a71d9fa67f8e3eff7f5a
Illia Raietskyi <rayetzkiillya@gmail.com>
2025-01-12 13:30:53 +0200

    Async Manual writable drain extra

--- async-start/stressor.js
+++ async-start/stressor.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 const { pipeline, finished } = require('stream/promises')
 const path = require('path')
+const { once } = require('events')
 const saxophonist = require('saxophonist')
 const { createBrotliDecompress } = require('zlib')
 const pLimit = require('p-limit')
@@ -26,33 +27,22 @@ async function parseAsync(file, writable) {
     highWaterMark: 1024 * 1024 * 1024 * 32
   })
 
-  async function * transform (stream) {
+  async function transform (stream) {
     for await (const chunk of stream) {
+      const waitDrain = !writable.write(chunkToRow(chunk))
       total++
-      yield chunkToRow(chunk)
+      if (waitDrain) {
+        await once(writable, 'drain')
+      }
     }
-    // TODO
-    // 1. iterate over each chunk
-    // 2. increment the total
-    // 3. transform the chunk using chunkToRow
-    // 4. yield the transformed chunk
   }
 
-  // TODO
-  // 1. create a pipeline from readable -> brotli -> parseStream -> transform -> writable
-  // 2. do not end the writable stream, so that it can be reused for the next parsed file
   await pipeline(
     readable,
     brotli,
     parseStream,
     transform,
-    writable,
-    { end: false }
   )
-  // TODO: Extra
-  // 1. call `writable.write()` directly inside the transform function
-  // 2. remove the yield and async generator function
-  // 3. handle backpressure by using the `events.once` utility and the 'drain' event
 
   console.timeEnd(file)
 }
@@ -62,22 +52,15 @@ async function start () {
   console.time('parsing time')
   console.log('Reading files: ', files)
 
-  // TODO
-  // 1. Create the write stream for pagesFilename
-  // 2. write the header to the file "id, title"
-  // 3. Iterate over each file and call parseAsync in *series*
-  // 3. End the writable stream and then wait on `finished(writeable)`
-  const pagesStream = fs.createWriteStream(pagesFilename)
+  const pagesStream = fs.createWriteStream(pagesFilename, {
+    highWaterMark: 1024 * 1,
+  })
   pagesStream.write('id,title\n')
-  
-  for (const file of files) {
-    await parseAsync(file, pagesStream)
-  }
 
-  // TODO: Extra - parallelise
-  // 1. Map over each file and call parseAsync
-  // 2. Promise.all that array of promises
-  // 3. Use `p-limit` to limit the concurrency
+  const limit = pLimit(2);
+  
+  const fileStream = files.map(file => limit(() => parseAsync(file, pagesStream)));
+  await Promise.all(fileStream);
 
   console.log('total: ', total)
   console.timeEnd('parsing time')
